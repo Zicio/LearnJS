7977d10b230562584ad2ebc7d75b47b3186777c6
Alexander Burachkov <zicio@yandex.ru>
2023-03-06 23:34:06 +0400

    Worked on Phase #2

--- 6-AdvancedWorkWithFunctions/9-call-apply-decorators/2/index.js
+++ 6-AdvancedWorkWithFunctions/9-call-apply-decorators/2/index.js
@@ -1,1 +1,17 @@
 "use strict";
+
+function delay(func, ms) {
+  return (...args) => {
+    setTimeout(() => func.apply(this, args), ms);
+  };
+}
+
+function f(x) {
+  console.log(x);
+}
+
+let f1000 = delay(f, 1000);
+let f2000 = delay(f, 2000);
+
+f1000("test1");
+f2000("test2");
